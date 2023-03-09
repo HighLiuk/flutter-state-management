22b2b188a3e4f8cb3434114b1da6408783683bbf
Luca Di Fazio <highliuk@gmail.com>
2023-03-09 23:02:00 +0100

    ♻️ Refactor to Counter Component

---
+++ lib/counter.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('InheritedWidget Demo'),
+      ),
+      body: Center(
+        child: Text(
+          '$_counter',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
